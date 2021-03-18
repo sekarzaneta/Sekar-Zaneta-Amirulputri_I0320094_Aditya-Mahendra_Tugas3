54bbc0f8a9766d3de63d90811c9bb6f096b68b75
Sekar Zaneta <masayusekar@gmail.com>
2021-03-18 17:23:54 +0700

    tugas 3

--- I0320094_soal1_tugas3.py
+++ I0320094_soal1_tugas3.py
@@ -1,1 +1,24 @@
+print("===== Identitas Teman =====")
 
+#membuat list teman
+list_teman = ['erika','sekar','hana','rafli','bonang','ano','yola','maurich','vizal','rio']
+print("list_teman[4]: ", list_teman[4])
+print("list_teman[6]: ", list_teman[6])
+print("list_teman[7]: ", list_teman[7])
+
+#mengganti list teman
+list_teman[3] = 'densur'
+list_teman[5] = 'titus'
+list_teman[9] = 'truely'
+
+#menambah list teman
+list_teman.extend(['riri','zafira'])
+
+#menampilkan list nama perulangan
+urutan = 0
+for list in range(0,12):
+    print(list_teman[urutan])
+    urutan = urutan + 1
+
+#menampilkan list
+print(len(list_teman))
--- I0320094_soal2_tugas3.py
+++ I0320094_soal2_tugas3.py
@@ -1,1 +1,21 @@
+print("==== Dictionary Identitas Diri ====")
 
+#Identitas Diri
+dictionary = {"Nama":["Sekar Zaneta Amirulputri"],
+              "Hobi":["Menonton film","Tidur","Membaca buku"],
+              "Sosial Media":["Instagram","Twitter","Line"],
+              "Lagu Kesukaan":["Heart-Shaped Box","Fiance","Stockholm Syndrome","Gang Berry"],
+              "Makanan Favorit":["Pempek","Dimsum","Tekwan"]}
+print(dictionary)
+
+#Mengubah dictionary
+dictionary["Hobi"][0]=("Makan")
+dictionary["Sosial Media"][2]=("Whatsapp")
+
+#Menghapus salah satu dictionary
+del dictionary["Makanan Favorit"][1:3]
+
+#Menambah dictionary
+dictionary["Hobi"].append("Mendengarkan lagu")
+
+print(dictionary)
